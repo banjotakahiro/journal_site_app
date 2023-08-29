25f5c5982a3c4a1802602f7bbb67723b55a63d6c
banjotakahiro <watawata20220404@gmail.com>
2023-08-29 22:49:25 +0900

    一覧と詳細が終わる

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Article;
 class ArticleController extends Controller
 {
-    //
+    public function index() {
+        $articles = Article::all();
+        return view('articles.index',['articles'=>$articles]);
+    }
+
+    public function show($id) {
+        $view_article = Article::find($id);
+        return view('articles.show',['view_article'=>$view_article]);
+    }
 }
