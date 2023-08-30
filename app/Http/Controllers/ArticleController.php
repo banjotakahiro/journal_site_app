bbedb364407232aa7a9eeaeed8889101d710b3c5
banjotakahiro <watawata20220404@gmail.com>
2023-08-30 16:15:26 +0900

    新規データの投稿が完成

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,17 +2,49 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Article;
+use Illuminate\Http\Request;
+
 class ArticleController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $articles = Article::all();
-        return view('articles.index',['articles'=>$articles]);
+        return view('articles.index', ['articles' => $articles]);
     }
 
-    public function show($id) {
+    public function show($id)
+    {
         $view_article = Article::find($id);
-        return view('articles.show',['view_article'=>$view_article]);
+        return view('articles.show', ['view_article' => $view_article]);
+    }
+
+    public function store(Request $request)
+    {
+        $article = new Article;
+        $article->title = $request->title;
+        $article->body = $request->body;
+
+        $article->save();
+        return redirect('/articles');
+    }
+
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    public function edit()
+    {
+    
+    }
+
+    public function update()
+    {
+    }
+
+    public function destroy()
+    {
+    
     }
 }
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>show</title>
 </head>
+
 <body>
     <h1>論文詳細</h1>
-    <h5>{{ $view_article->title }}</h5>
-    <h5>{{ $view_article->body }}</h5>
+    <p>タイトル:{{ $view_article->title }}</p>
+    <p>{{ $view_article->body }}</p>
     <button onclick="location.href='/articles'">一覧へ戻る</button>
 </body>
+
 </html>
